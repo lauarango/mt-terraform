67770507ddb2c3cf2c3a5d50f84cf8da44200a71
lauarango <88212130+lauarango@users.noreply.github.com>
2024-09-12 22:06:23 -0500

    Update main.tf

---
+++ terraform-code/main.tf
@@ -0,0 +1,27 @@
+resource "random_id" "random" {
+	byte_length = 2
+	count = 2
+}
+
+resource "github_repository" "mtc_repo" {
+	count = 2
+    name = "mtc_repo-${random_id.random[count.index].dec}"
+    description = "MTC Code"
+    visibility = "private"
+    auto_init = true
+}
+
+ resource "github_repository_file" "readme"{
+ 	repository = github_repository.mtc_repo[count.index].name
+ 	branch = "main"
+ 	file = "README.md"
+ 	content = " # This repository is for infra developers. "
+ 	overwrite_on_create = true 
+ }
+  resource "github_repository_file" "index"{
+  	repository = github_repository.mtc_repo[count.index].name
+  	branch = "main"
+  	file = "index.html"
+  	content = "Hello Terraform!"
+  	overwrite_on_create = true 
+  }
